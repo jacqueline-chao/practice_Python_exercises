abaf97540dd782ce2031eb253150f4d0e1b93d3c
Jacqueline Chao <jacqc2102@gmail.com>
2023-12-25 11:40:44 -0500

    Add Exercise 1: Character Input

---
+++ 01_character-input/script.py
@@ -0,0 +1,13 @@
+user_name = input('Please enter your first name. ')
+user_age = int(input('Please enter your age in years. '))
+user_number = int(input('Please enter a number. '))
+
+curr_year = 2023
+rem_years = 100 - user_age
+
+hund_year = curr_year + rem_years
+
+text = 'You, ' + user_name + ', will turn 100 years old in the year ' + str(hund_year) + '.'
+
+for i in range(user_number):
+    print(text)
